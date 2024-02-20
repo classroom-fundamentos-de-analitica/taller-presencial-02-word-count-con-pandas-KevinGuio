fee73de716c54626d8067ea1a29a75ded6ab3491
Kevin Guio <kguio@unal.edu.co>
2024-02-19 22:51:37 -0500

    Entrega trabajo

--- word_count.py
+++ word_count.py
@@ -6,12 +6,20 @@ import pandas as pd
 
 
 def load_input(input_directory):
-    """Load text files in 'input_directory/'"""
-    #
+  #
     # Lea los archivos de texto en la carpeta input/ y almacene el contenido en
     # un DataFrame de Pandas. Cada línea del archivo de texto debe ser una
     # entrada en el DataFrame.
     #
+  """Load text files in 'input_directory/'"""
+  filenames = glob.glob(input_directory + '/*.*')
+  dataframes = [
+      pd.read_csv(filename, index_col = None, sep = ";", names = ['text']) for filename in filenames
+  ]
+
+  dataframe = pd.concat(dataframes).reset_index(drop = True)
+  return dataframe
+    
 
 
 def clean_text(dataframe):
@@ -19,14 +27,33 @@ def clean_text(dataframe):
     #
     # Elimine la puntuación y convierta el texto a minúsculas.
     #
+    dataframe = dataframe.copy()
+
+
+
+    dataframe['text'] = dataframe['text'].str.lower()
+    dataframe['text'] = dataframe['text'].str.replace(",","").str.replace(".","")
+    return dataframe
 
 
 def count_words(dataframe):
     """Word count"""
 
+    dataframe = dataframe.copy()
+    dataframe['text'] = dataframe['text'].str.split()
+    dataframe = dataframe.explode('text').reset_index(drop = True)
+    dataframe = dataframe.rename(columns = {'text': 'word'})
+    dataframe['count'] = 1
+
+    conteo = dataframe.groupby(['word'], as_index = False).agg({'count':sum})
+
+    return conteo
+
 
 def save_output(dataframe, output_filename):
     """Save output to a file."""
+
+    dataframe.to_csv(output_filename, index = False, sep = "\t", header = False)
 
 
 #
@@ -34,6 +61,11 @@ def save_output(dataframe, output_filename):
 #
 def run(input_directory, output_filename):
     """Call all functions."""
+    dataframe = load_input(input_directory)
+    dataframe = clean_text(dataframe)
+    dataframe = count_words(dataframe)
+    save_output(dataframe, output_filename)
+
 
 
 if __name__ == "__main__":
